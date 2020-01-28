770a96b88478d1ade83060713c1773ae228521f7
Ahmed Zaher <ahmedzaher1080@gmail.com>
2020-01-28 18:22:07 +0200

    Added CF688-D2-D

---
+++ CodeForces/CF688-D2-D.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int OO = 1e9;
+const double EPS = 1e-9;
+
+#define ndl cout << '\n'
+#define sz(v) int(v.size())
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define present(a, x) (a.find(x) != a.end())
+#ifdef LOCAL
+#define db(...) ({cout << "> Line " << __LINE__  \
+		<< ": "; _db(#__VA_ARGS__, __VA_ARGS__);})
+#define RNG() rng()
+#else
+#define db(...) true
+#define RNG() true
+#endif
+
+template<class T>
+void _db(const char *dbStr, T e) {
+	cout << dbStr << " = " << e << endl;
+}
+template<class T, class... L>
+void _db(const char *dbStr, T e, L... r) {
+  while(*dbStr != ',') cout << *dbStr++;
+  cout << " = " << e << ','; _db(dbStr + 1, r...);
+}
+template<class S, class T>
+ostream& operator<<(ostream& o, const map<S, T>& v) {
+	o << "["; int i = 0;
+	for (const pair<S, T>& pr : v) o << (!i++ ? "" : ", ") << "{"
+	<< pr.fs << " : " << pr.sc << "}"; return o << "]";
+}
+template<template <class, class...> class S, class T, class... L>
+ostream& operator<<(ostream& o, const S<T, L...>& v) {
+	o << "["; int i = 0;
+	for (const auto& e : v) o << (!i++ ? "" : ", ") << e;
+	return o << "]";
+}
+template<class S, class T>
+ostream& operator<<(ostream& o, const pair<S, T>& pr) {
+	return o << "(" << pr.fs << ", " << pr.sc << ")";
+}
+ostream& operator<<(ostream& o, const string& s) {
+	for (const char& c : s) o << c;
+	return o;
+}
+
+template<class T> using V = vector<T>;
+template<class T> using VV = V<V<T>>;
+template<class T> using VVV = VV<V<T>>;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = V<int>;
+using vii = V<pii>;
+using vvi = VV<int>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using si = set<int>;
+using usi = unordered_set<int>;
+
+
+vi PFs;
+void getPFs(ll x) {
+	for (ll i = 2; i * i <= x; ++i)
+		if (x % i == 0) {
+			PFs.pb(1);
+			while (x % i == 0)
+				PFs.back() *= i, x /= i;
+		}
+	if (x > 1)
+		PFs.pb(x);
+}
+
+
+int main() {
+	#ifdef LOCAL
+	auto stTime = clock();
+//	freopen("in.txt", "r", stdin);
+	mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+	#endif
+	ios::sync_with_stdio(false);
+	cout.precision(10);
+	cin.tie(0);
+
+	int n, k;
+
+	cin >> n >> k;
+
+	getPFs(k);
+	V<bool> good(sz(PFs));
+
+	for (int i = 0; i < n; ++i) {
+		int c;
+
+		cin >> c;
+
+		for (int j = 0; j < sz(PFs); ++j) {
+			if (c % PFs[j] == 0)
+				good[j] = true;
+		}
+
+	}
+
+	for (int i = 0; i < sz(good); ++i) {
+		if (!good[i]) {
+			cout << "No\n";
+			return 0;
+		}
+	}
+
+	cout << "Yes\n";
+
+
+	#ifdef LOCAL
+	cout << "\n\n\nExecution time: " <<
+		(clock() - stTime) * 1e3 / CLOCKS_PER_SEC << " ms" << endl;
+	#endif
+    return 0;
+}

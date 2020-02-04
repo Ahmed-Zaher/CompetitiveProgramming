b85934c70b8e7e825ca4346dd2416872d9bb05c1
Ahmed Zaher <ahmedzaher1080@gmail.com>
2020-02-04 02:41:57 +0200

    AtCoder079-ARC-B

---
+++ AtCoder/AtCoder079-ARC-B.cpp
@@ -0,0 +1,121 @@
+/*
+ * Note that if we have a sequence a1, a2, ..., aN
+ * and max ai - min ai <= N, and assuming that ai >= N,
+ * then after N moves the resulting sequence will be
+ * a1 - 1, a2 - 1, ..., aN - 1
+ * Now let's use N = 50, and consider the sequence 1 2 .. 50,
+ * we can apply 50 moves to this sequence to get 0 1 .. 49 and then
+ * we're out of moves.
+ * Now let's get K % 50 moves by using the sequence 0..49 after incrementing
+ * the last K % 50 elements, and get K / 50 moves by incrementing the whole
+ * sequence by K / 50 - we can observe that such solution always works
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int OO = 1e9;
+const double EPS = 1e-9;
+
+#define ndl cout << '\n'
+#define sz(v) int(v.size())
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define present(a, x) (a.find(x) != a.end())
+#ifdef LOCAL
+#define db(...) ({cout << "> Line " << __LINE__  \
+		<< ": "; _db(#__VA_ARGS__, __VA_ARGS__);})
+#define RNG() rng()
+#else
+#define db(...) true
+#define RNG() true
+#endif
+
+template<class T>
+void _db(const char *dbStr, T e) {
+	cout << dbStr << " = " << e << endl;
+}
+template<class T, class... L>
+void _db(const char *dbStr, T e, L... r) {
+  while(*dbStr != ',') cout << *dbStr++;
+  cout << " = " << e << ','; _db(dbStr + 1, r...);
+}
+template<class S, class T>
+ostream& operator<<(ostream& o, const map<S, T>& v) {
+	o << "["; int i = 0;
+	for (const pair<S, T>& pr : v) o << (!i++ ? "" : ", ") << "{"
+	<< pr.fs << " : " << pr.sc << "}"; return o << "]";
+}
+template<template <class, class...> class S, class T, class... L>
+ostream& operator<<(ostream& o, const S<T, L...>& v) {
+	o << "["; int i = 0;
+	for (const auto& e : v) o << (!i++ ? "" : ", ") << e;
+	return o << "]";
+}
+template<class S, class T>
+ostream& operator<<(ostream& o, const pair<S, T>& pr) {
+	return o << "(" << pr.fs << ", " << pr.sc << ")";
+}
+ostream& operator<<(ostream& o, const string& s) {
+	for (const char& c : s) o << c;
+	return o;
+}
+
+template<class T> using V = vector<T>;
+template<class T> using VV = V<V<T>>;
+template<class T> using VVV = VV<V<T>>;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = V<int>;
+using vii = V<pii>;
+using vvi = VV<int>;
+using mii = map<int, int>;
+using umii = unordered_map<int, int>;
+using si = set<int>;
+using usi = unordered_set<int>;
+
+
+
+
+
+int main() {
+	#ifdef LOCAL
+	auto stTime = clock();
+//	freopen("in.txt", "r", stdin);
+	mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+	#endif
+	ios::sync_with_stdio(false);
+	cout.precision(10);
+	cin.tie(0);
+
+	ll K;
+
+	cin >> K;
+
+	cout << "50\n";
+
+	V<ll> ans(50, 0);
+
+	for (int i = 0; i < 50; ++i) {
+		ans[i] = i + (K / 50);
+	}
+
+	for (int i = 50 - (K % 50); i < 50; ++i) {
+		++ans[i];
+	}
+
+	for (int i = 0; i < 50; ++i) {
+		cout << ans[i] << " \n"[i + 1 == 50];
+	}
+
+
+
+	#ifdef LOCAL
+	cout << "\n\n\nExecution time: " <<
+		(clock() - stTime) * 1e3 / CLOCKS_PER_SEC << " ms" << endl;
+	#endif
+    return 0;
+}
